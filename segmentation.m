297927365c47e7f06acb85d369c39a91bdc2af24
Merics Ferenc <mericsferenc@gmail.com>
2023-11-28 15:48:25 +0100

    test LoG filter

--- segmentation.m
+++ segmentation.m
@@ -1,26 +1,42 @@
 
-inputImage = imread('./original_images/tif/02_test.tif');
+inputImage = imread('./original_images/tif/01_test.tif');
 
 greenChannel = inputImage(:,:,2);
-I_enhanced = adapthisteq(greenChannel);
-I_masked = I_enhanced;
-I_masked(~mask) = 0;
 
-se = strel('disk', 15);
-I_top_hat = imtophat(I_masked, se);
+I_smooth = imgaussfilt(greenChannel, 2);
 
-level = graythresh(I_top_hat);
-I_otsu = imbinarize(I_top_hat, level);
+I_enhanced = adapthisteq(I_smooth);
 
-I_cleaned = bwareaopen(I_otsu, 30);
+h = fspecial('log', [5 5], 0.5);
 
-se2 = strel('disk', 3);
-I_thick_vessels = imclose(I_cleaned, se2);
+I_log = imfilter(I_enhanced, h, 'replicate');
 
-I_thin_vessels = I_top_hat & ~I_thick_vessels;
+I_enhanced_edges = I_enhanced - I_log;
 
-I_final_vessels = I_thick_vessels | I_thin_vessels;
+I_sharpened = imsharpen(I_enhanced_edges, 'Radius', 1, 'Amount', 4);
+
+thresholdValue = graythresh(I_sharpened);
+I_binarized_sharpened = imbinarize(I_sharpened, thresholdValue);
+
+
+
+
+se = strel('disk', 1);
+I_dilated = imdilate(I_binarized_sharpened, se);
+I_temp_closed = imerode(I_dilated, se);
+I_thinned = bwmorph(I_temp_closed, 'thin', 5);
+I_dist_transform = bwdist(~I_thinned);
+I_cond_dilated = imdilate(I_thinned, strel('disk', 1)) & (I_dist_transform < 4);
+I_spur_removed = bwmorph(I_cond_dilated, 'spur', 5); 
+I_final_inverted = ~I_spur_removed;
+
 
 figure;
-subplot(1,2,1), imshow(inputImage), title('Original Image');
-subplot(1,2,2), imshow(I_final_vessels), title('Segmented');
+subplot(2,3,1), imshow(inputImage), title('Original Image');
+subplot(2,3,2), imshow(I_enhanced), title('Enhanced Image');
+subplot(2,3,3), imshow(I_enhanced_edges), title('LoG Filtered Image');
+subplot(2,3,4), imshow(I_sharpened), title('Sharpened Image');
+subplot(2,3,5), imshow(I_binarized_sharpened), title('Binarized Image');
+subplot(2,3,6), imshow(I_final_inverted), title('Final Inverted Vessels');
+
+imwrite(I_final_inverted, 'final_inverted_vessels.png');
