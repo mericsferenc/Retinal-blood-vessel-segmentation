dddbb273fdb248e2370c2f3ceb0c5ef50136653d
Merics Ferenc <mericsferenc@gmail.com>
2023-11-28 16:08:23 +0100

    denoising and log filtering test

--- segmentation.m
+++ segmentation.m
@@ -1,42 +1,26 @@
-
-inputImage = imread('./original_images/tif/01_test.tif');
+inputImage = imread('01_test.tif');
 
 greenChannel = inputImage(:,:,2);
 
-I_smooth = imgaussfilt(greenChannel, 2);
+I_denoised = medfilt2(greenChannel, [3 3]);
 
-I_enhanced = adapthisteq(I_smooth);
+I_enhanced = adapthisteq(I_denoised);
 
-h = fspecial('log', [5 5], 0.5);
+sigma = 2;
+hsize = 15;
+I_log = fspecial('log', hsize, sigma);
+I_edge = imfilter(I_enhanced, I_log, 'same', 'replicate');
+I_edge = mat2gray(abs(I_edge));
 
-I_log = imfilter(I_enhanced, h, 'replicate');
-
-I_enhanced_edges = I_enhanced - I_log;
-
-I_sharpened = imsharpen(I_enhanced_edges, 'Radius', 1, 'Amount', 4);
+I_sharpened = imsharpen(I_edge, 'Radius', 1, 'Amount', 1.5);
 
 thresholdValue = graythresh(I_sharpened);
-I_binarized_sharpened = imbinarize(I_sharpened, thresholdValue);
-
-
-
-
-se = strel('disk', 1);
-I_dilated = imdilate(I_binarized_sharpened, se);
-I_temp_closed = imerode(I_dilated, se);
-I_thinned = bwmorph(I_temp_closed, 'thin', 5);
-I_dist_transform = bwdist(~I_thinned);
-I_cond_dilated = imdilate(I_thinned, strel('disk', 1)) & (I_dist_transform < 4);
-I_spur_removed = bwmorph(I_cond_dilated, 'spur', 5); 
-I_final_inverted = ~I_spur_removed;
-
+I_binarized = imbinarize(I_sharpened, thresholdValue);
 
 figure;
-subplot(2,3,1), imshow(inputImage), title('Original Image');
-subplot(2,3,2), imshow(I_enhanced), title('Enhanced Image');
-subplot(2,3,3), imshow(I_enhanced_edges), title('LoG Filtered Image');
-subplot(2,3,4), imshow(I_sharpened), title('Sharpened Image');
-subplot(2,3,5), imshow(I_binarized_sharpened), title('Binarized Image');
-subplot(2,3,6), imshow(I_final_inverted), title('Final Inverted Vessels');
-
-imwrite(I_final_inverted, 'final_inverted_vessels.png');
+subplot(2,4,1), imshow(inputImage), title('Original Image');
+subplot(2,4,2), imshow(I_denoised), title('Denoised Image');
+subplot(2,4,3), imshow(I_enhanced), title('Enhanced Image');
+subplot(2,4,4), imshow(I_edge), title('LoG Filtered Image');
+subplot(2,4,5), imshow(I_sharpened), title('Sharpened Image');
+subplot(2,4,6), imshow(I_binarized), title('Binarized Image');
