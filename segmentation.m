375e641b33ad2b50226d937335ddc06466385a91
Merics Ferenc <mericsferenc@gmail.com>
2023-11-28 13:52:53 +0100

    apply gauss filter and adaptive histogram equalization

--- segmentation.m
+++ segmentation.m
@@ -1,4 +1,12 @@
 inputImage = imread('./original_images/tif/01_test.tif');
 
+
+
+greenChannel = inputImage(:,:,2);
+I_smooth = imgaussfilt(greenChannel, 2); 
+
+I_enhanced = adapthisteq(I_smooth);
+
 figure;
 subplot(1,1,1), imshow(inputImage), title('Original Image');
+subplot(1,1,2), imshow(I_enhanced), title('Original Image');
