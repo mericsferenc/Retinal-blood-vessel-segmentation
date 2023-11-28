813277e7a2f2af6ba4fb30f3a61ac40bd45d69fa
Merics Ferenc <mericsferenc@gmail.com>
2023-11-28 14:01:38 +0100

    change figure names

--- segmentation.m
+++ segmentation.m
@@ -1,7 +1,10 @@
-inputImage = imread('./original_images/tif/01_test.tif'); % Replace with your actual file name
+
+inputImage = imread('./original_images/tif/01_test.tif');
 
 greenChannel = inputImage(:,:,2);
-I_smooth = imgaussfilt(greenChannel, 2); % Adjust the sigma value as needed
+
+I_smooth = imgaussfilt(greenChannel, 2);
+
 I_enhanced = adapthisteq(I_smooth);
 
 orientations = 0:20:160;
@@ -16,12 +19,12 @@ end
 
 I_matched_small_normalized = mat2gray(I_matched_small);
 
-I_sharpened = imsharpen(I_matched_small_normalized, 'Radius', 2, 'Amount', 4);
+I_sharpened = imsharpen(I_matched_small_normalized, 'Radius', 1, 'Amount', 4);
 
 thresholdValue = graythresh(I_sharpened); 
 I_binarized_sharpened = imbinarize(I_sharpened, thresholdValue);
 
-se = strel('disk', 2);
+se = strel('disk', 1);
 I_dilated = imdilate(I_binarized_sharpened, se);
 I_temp_closed = imerode(I_dilated, se);
 
@@ -33,15 +36,15 @@ I_cond_dilated = imdilate(I_thinned, strel('disk', 1)) & (I_dist_transform < 4);
 
 I_spur_removed = bwmorph(I_cond_dilated, 'spur', 5); 
 
+I_final_inverted = ~I_spur_removed;
 
 figure;
-subplot(3,4,1), imshow(inputImage), title('img 1');
-subplot(3,4,2), imshow(I_matched_small_normalized, []), title('img 2');
-subplot(3,4,3), imshow(I_sharpened, []), title('img 3');
-subplot(3,4,4), imshow(I_binarized_sharpened), title('img 4');
-subplot(3,4,5), imshow(I_temp_closed), title('img 5');
-subplot(3,4,6), imshow(I_thinned), title('img 6');
-subplot(3,4,7), imshow(I_cond_dilated), title('img 7');
-subplot(3,4,8), imshow(I_spur_removed), title('img 8');
-
-
+subplot(3,4,1), imshow(inputImage), title('Original Image');
+subplot(3,4,2), imshow(I_matched_small_normalized, []), title('Normalized Small Vessels Gabor Response');
+subplot(3,4,3), imshow(I_sharpened, []), title('Sharpened Small Vessels Response');
+subplot(3,4,4), imshow(I_binarized_sharpened), title('Binarized Sharpened Image');
+subplot(3,4,5), imshow(I_temp_closed), title('Temporarily Closed Image');
+subplot(3,4,6), imshow(I_thinned), title('Thinned Image');
+subplot(3,4,7), imshow(I_cond_dilated), title('Conditionally Dilated Vessels');
+subplot(3,4,8), imshow(I_spur_removed), title('Segmented Blood Vessels After Spur Removal');
+subplot(3,4,9), imshow(I_final_inverted), title('Final Inverted Vessels');
